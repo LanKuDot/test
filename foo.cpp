d06120bb63ec6edf9fab2da63efee7c330bf3e75
LanKuDot <lankudot@github.com>
2014-04-06 21:53:56 +0800

    Create add3num() func

--- foo.cpp
+++ foo.cpp
@@ -13,6 +13,11 @@ int multi2num( int a, int b )
 	return a * b;
 }
 
+int add3num( int a, int b, int c )
+{
+	return a + b + c;
+}
+
 int main()
 {
 	int result = sum2num( 2, 14 ) + minus2num( 2, 14 ) + multi2num( 2, 14 );
